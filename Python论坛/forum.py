061370b195519e1e406bcbc60455c82fa4937230
Bigstart <1715755221@qq.com>
2023-08-30 12:25:35 +0800

    update 注释

--- Python论坛/forum.py
+++ Python论坛/forum.py
@@ -1,22 +1,30 @@
+#导入必要的库和模块
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
+#创建Flask应用实例
 app = Flask(__name__)
+#配置数据库,使用SQLite数据库
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
+#设置秘钥,用于session加密
 app.config['SECRET_KEY'] = 'my_secret_key'
+#为应用创建SQLAlchemy实例
 db = SQLAlchemy(app)
 
 
+#定义用户模型
 class User(UserMixin, db.Model):
+    #设定数据表
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 
+#定义帖子模型
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -25,23 +33,27 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     user = db.relationship("User", backref="posts")
 
-
+# 实例化一个 LoginManager 对象
 login_manager = LoginManager()
+# 设置登录视图函数名称，这将用作登录要求的重定向页面
 login_manager.login_view = "login"
+# 初始化登陆管理对象
 login_manager.init_app(app)
 
 
+# 用户加载函数，用于获取用户
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
+# 首页路由，展示所有帖子
 @app.route('/')
 def index():
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html', posts=posts)
 
 
+# 登录路由
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -58,6 +70,7 @@ def login():
     return render_template('login.html')
 
 
+# 注册路由
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -77,6 +90,7 @@ def register():
     return render_template('register.html')
 
 
+# 登出路由
 @app.route('/logout')
 @login_required
 def logout():
@@ -84,6 +98,7 @@ def logout():
     return redirect('/')
 
 
+# 新建帖子路由
 @app.route('/new_post', methods=['GET', 'POST'])
 @login_required
 def new_post():
@@ -97,6 +112,7 @@ def new_post():
     return render_template('new_post.html')
 
 
+# 删除帖子路由
 @app.route('/delete_post/<int:post_id>', methods=['POST'])
 @login_required
 def delete_post(post_id):
@@ -109,8 +125,10 @@ def delete_post(post_id):
         return render_template('index.html', posts=Post.query.all(), error="您没有权限执行此操作")
 
 
+# 在应用上下文中创建表格
 with app.app_context():
     db.create_all()
 
+# 启动应用
 if __name__ == "__main__":
     app.run(debug=True)
